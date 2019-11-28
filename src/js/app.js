bf7f5ca77a839cf7132b15a125cdb628e3376f39
Olavo Mello <olavo@agenciadix.com.br>
2019-11-28 03:22:57 -0300

    Update

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function(){  
+    var socket = io.connect("http://localhost:3000");
+    var ready = false;
+
+    // Join
+    ready = true;
+    socket.emit("join", "Olavo");
+	socket.emit("send", "Entrei...");
+
+    socket.on("update", function(msg) {
+    	$("#main").append('<li class="info">' + msg + '</li>')
+    }); 
+
+    socket.on("chat", function(client,msg) {
+        var time = new Date();
+        $("#main").append('<li class="field"><div class="msg"><span>' + client + ':</span><p>' + msg + '</p><time>' + time.getHours() + ':' + time.getMinutes() + '</time></div></li>');
+    });
+});
--- src/js/server.js
+++ src/js/server.js
@@ -1,21 +1,26 @@
-﻿const cluster       =   require('cluster');
-const http          =   require('http');
-const OS            =   require('os');
+﻿const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const cluster = require('cluster');
+const OS = require('os');
 
 // Server info
-const hostName      =   OS.hostname;
-const numCPUs       =   OS.cpus().length;
-const numMem        =   OS.freemem;
-const numUpTime     =   OS.uptime;
-const strPlataform  =   OS.platform;
+const hostName = OS.hostname;
+const numCPUs = OS.cpus().length;
+const numMem = OS.freemem;
+const numUpTime = OS.uptime;
+const strPlataform = OS.platform;
+const PORT = 3000;
 
 // Vars
-var strFullLog      =   "";
+var strFullLog = "";
+
+var clients = {}; 
 
 // Function log
 function log( strLog ){
     console.log(strLog);
-    strFullLog +=   strLog;
+    //strFullLog +=   `${strLog}\n`;
 }
 
 if (cluster.isMaster) {
@@ -38,25 +43,54 @@ if (cluster.isMaster) {
 
     // Online
     cluster.on('online', (worker, code, signal) => {
-        log(`worker ${worker.process.pid} is online`);
+        log(`Cluster ${worker.process.pid} is online`);
     });
-    // Online
+    // Offline
     cluster.on('disconnect', (worker, code, signal) => {
-        log(`worker ${worker.process.pid} is offline`);
+        log(`Cluster ${worker.process.pid} is offline`);
     });   
     // On exit
     cluster.on('exit', (worker, code, signal) => {
-        log(`worker ${worker.process.pid} died`);
+        log(`Cluster ${worker.process.pid} died`);
     });
 } else {
-  
-    http.createServer((req, res) => {
-        // Print infos
-        //res.writeHead(200, {"Content-Type": "text/plain"});
-        res.writeHead(200);
-        res.end(strFullLog);
-    }).listen(8000);
-
-  // Infos
-  // console.log(`Worker ${process.pid} started`);
+    // Clusters / Threads
+    http.listen(PORT, function(){
+        
+    });
 }
+
+// Routes
+app.get('/', function(req, res){
+    res.send('server is running');
+});
+io.on("connection", function (client) {  
+    console.log("New connection");
+    
+    // ----------------------------------------------
+    // Send commands to clients
+    // ----------------------------------------------
+    // Join
+    client.on("join", function(name){
+        console.log("Joined: " + name);
+        clients[client.id] = name;
+        client.emit("update", "Connected.");
+        client.broadcast.emit("update", name + " has joined the server.")
+    });
+    // Send
+    client.on("send", function(msg){
+        console.log("Message: " + msg);
+        client.broadcast.emit("chat", clients[client.id], msg);
+    });
+    // Disconect
+    client.on("disconnect", function(){
+        console.log("Disconnect");
+        io.emit("update", clients[client.id] + " has left the server.");
+        delete clients[client.id];
+    });
+    // ----------------------------------------------
+});
+
+
+// 
+console.log(`Server listen on port : ${PORT}`);
